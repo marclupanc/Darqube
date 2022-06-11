10fe041b2c8542b7ceee42dfd919dc6db02564fa
Marac Lupanciuc <maraclupanciuc@Maracs-MacBook-Pro.local>
2022-06-11 18:47:22 +0300

    Darqube News app. React Test from Julian. Starting the project. Main folder structure

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,38 @@
+// import PostItem from "../../components/PostItem";
+// import LatestItem from "../../components/LatestItem";
+import {useEffect, useState} from "react";
+
+const Home = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch('https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2021-03-01&to=2021-03-15&token=bpjsf67rh5r9328ecgvg')
+      .then(response => response.json())
+      .then(responseData => setNews(responseData))
+  }, [])
+
+  return (
+    <div className='container'>
+      <div className="row">
+        <div className="col-xl-4 col-lg-6 col-md-6 pb-4 latest">
+          {<LatestItem latestPostData={news}/>}
+        </div>
+        <div className="col-xl-8 col-lg-6 col-md-6 ">
+          <div className="row phones">
+            {news.slice(1, 7).map((data) => <PostItem postData={data}/>)}
+            <div className="row pagination">
+              <div className="page-navigation pb-4">
+                <div className="pages">1-6 out of 24</div>
+                <div className="pages-btn">
+                  <button className="previous">Previous</button>
+                  <button className="next">Next</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
